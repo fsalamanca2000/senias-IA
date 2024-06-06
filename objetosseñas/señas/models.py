66c18fcc148f716ce53b95856c6da2186f96a6b8
Felipe Salamanca González <55007647+fsalamanca2000@users.noreply.github.com>
2024-06-06 16:33:44 -0500

    Add files via upload

--- objetosseñas/señas/models.py
+++ objetosseñas/señas/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class RecognizedObject(models.Model):
-    name = models.CharField(max_length=100)
-    timestamp = models.DateTimeField(auto_now_add=True)
